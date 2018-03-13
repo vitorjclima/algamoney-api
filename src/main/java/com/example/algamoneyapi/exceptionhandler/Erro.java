ba0fe0404c9e41bafa1305295dafb86d1951ce69
Vitor Lima <vitor.lima@msn.com>
2018-03-12 23:47:07 -0300

    Handling Errors with ExceptionHandler

---
+++ src/main/java/com/example/algamoneyapi/exceptionhandler/Erro.java
@@ -0,0 +1,33 @@
+package com.example.algamoneyapi.exceptionhandler;
+
+/**
+ * @author vitor
+ *
+ * created on 2018-03-12
+ */
+public class Erro {
+
+    private String mensagemUsuario;
+    private String mensagemDesenvolvedor;
+
+
+    public Erro(String mensagemUsuario, String mensagemDesenvolvedor) {
+        this.mensagemUsuario = mensagemUsuario;
+        this.mensagemDesenvolvedor = mensagemDesenvolvedor;
+    }
+    /**
+     * @return the mensagemUsuario
+     */
+    public String getMensagemUsuario() {
+        return mensagemUsuario;
+    }
+    /**
+     * @return the mensagemDesenvolvedor
+     */
+    public String getMensagemDesenvolvedor() {
+        return mensagemDesenvolvedor;
+    }
+
+
+
+}
